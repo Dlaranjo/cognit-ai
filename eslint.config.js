ecda6a25036310f35bebcb39c52e63c41382d8a7
Daniel Laranjo <dglaranjo@hotmail.com>
2025-07-21 10:54:49 -0300

    Resolvendo vulnerabilidades

--- eslint.config.js
+++ eslint.config.js
@@ -23,6 +23,15 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      // Fix for @typescript-eslint/no-unused-expressions rule
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        {
+          allowShortCircuit: true,
+          allowTernary: true,
+          allowTaggedTemplates: true,
+        },
+      ],
     },
   }
 );
